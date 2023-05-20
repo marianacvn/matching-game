f84e1b169bdf823706b83750e541d8ce47aca859
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-20 13:56:46 -0300

    :bento: Update images

--- src/components/CardGame/index.jsx
+++ src/components/CardGame/index.jsx
@@ -4,7 +4,7 @@ import './styles.css';
 function CardGame() {
     return `
     <article class="card-game">
-        <img src="../../images/alurapixel.png" alt="Logo da Alura">
+        <img src="../../../src/images/alurapixel.png" alt="Logo da Alura">
     </article>
     `;
 }
