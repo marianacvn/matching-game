ad02810fb1280292dd27f9677e3d97b9ecec27f4
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-20 14:19:21 -0300

    :art: Criação do BoardGame

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import './styles/generic/reset.css';
 import './styles/settings/colors.css';
 import './styles/elements/base.css';
-import CardGame from "./components/CardGame";
+import BoardGame from './objects/BoardGame';
+
 
 
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
