d8b65db15ceab7e11499a0a70bbf9eb0c0fbfcb1
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-23 13:10:50 -0300

    :construction: Create Componenet CardFrontBack without Css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import './styles/generic/reset.css';
 import './styles/settings/colors.css';
 import './styles/elements/base.css';
-import BoardGame from './objects/BoardGame';
 import ScoreBoard from './objects/ScoreBoard';
+import BoardGame from './objects/BoardGame';
 
 const $root = document.querySelector("#root");
 
@@ -10,7 +10,7 @@ $root.insertAdjacentHTML(
     "beforeend", 
     ` 
         ${ScoreBoard()}
-        ${BoardGame(6)}
+        ${BoardGame(2)}
     `
 );
  
