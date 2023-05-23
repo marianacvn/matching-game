c2cf7d1ff15826351953c3baade219ff6d7874d3
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-23 14:51:23 -0300

    :sparkles: Create Event Click with CardFrontBack

--- src/components/CardFrontBack/index.jsx
+++ src/components/CardFrontBack/index.jsx
@@ -1,9 +1,17 @@
 import CardGame from '../CardGame/index';
 import './styles.css';
 
-function CardFrontBack (){ 
+function CardFrontBack (){  
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+       const $origin = event.target;
+       const $cardFrontBack = $origin.closest('.card-front-back');
+
+       $cardFrontBack.classList.toggle('-active');
+    } 
+
     return /*html*/ `
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
