b21bfa47f2dfbf0b395a2fbb34e6f3998952ade1
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-24 20:39:22 -0300

    :memo: Create the javascript, html and css cards

--- src/components/CardFrontBack/index.jsx
+++ src/components/CardFrontBack/index.jsx
@@ -1,7 +1,7 @@
 import CardGame from '../CardGame/index';
 import './styles.css';
 
-function CardFrontBack (){  
+function CardFrontBack (icon, altIcon){  
     window.cardFrontBack = {}
     window.cardFrontBack.handleClick = (event) => {
        const $origin = event.target;
@@ -16,7 +16,7 @@ function CardFrontBack (){
                 ${CardGame()}
             </div>
             <div class="card -back">
-                ${CardGame("javascript", 'Logo do javascript')}
+                ${CardGame(icon, altIcon)}
             </div>
         </article>
     `;
---
+++ src/objects/BoardGame/data.jsx
@@ -0,0 +1,26 @@
+
+const cards = [{
+    icon: "javascript",
+    altIcon: "Logo do Javascript"
+},
+{
+    icon: "javascript",
+    altIcon: "Logo do Javascript"
+},
+{
+    icon: "html",
+    altIcon: "Logo do HTML"
+},
+{
+    icon: "html",
+    altIcon: "Logo do HTML"
+},
+{
+    icon: "css",
+    altIcon: "Logo do CSS"
+},
+{   icon: "css",
+altIcon: "Logo do CSS"
+}];
+
+export default cards;
