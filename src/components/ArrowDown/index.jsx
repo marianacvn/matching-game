334fa3524bb117393471c3b16a41361480480c4d
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-25 12:47:43 -0300

    :ambulance: Swap player when flip two cards

--- src/components/ArrowDown/index.jsx
+++ src/components/ArrowDown/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 function ArrowDown (currentPlayer = 1) {
     return /*html*/`
         <img class="arrow-down" 
-            data-currentPlayer="${currentPlayer}"
+            data-currentplayer="${currentPlayer}"
             src="../../../src/images/arrowdown.png"
             alt="Ícone de uma seta para baixo">
     `;
